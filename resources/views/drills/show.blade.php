e555cd12fc2ef08dc9845f0f386f5b6025f59d4d
Hiroaki Kitagawa <hirock17l@icloud.com>
2019-08-18 05:54:19 +0900

    vue.js実装,全問解答で終了するよう改修

--- resources/views/drills/show.blade.php
+++ resources/views/drills/show.blade.php
@@ -1,18 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-md-8">
-        <div class="card">
-        <div class="card-header">{{ __('Practice'),'「'.$drill->title.'」'}}</div>
-        </div>
-        <div class="card-body text-center">
-        <p>{{ $drill->problem0 }}</p>
-        <p>{{ $drill->problem1 }}</p>
-        <p>{{ $drill->problem2 }}</p>
-        </div>
-      </div>
-    </div>
+  <div id="app">
+    {{-- デフォルトだとこの中ではvue.jsが有効 --}}
+    {{-- example-componentはLaraveln入っているサンプルのコンポートネント --}}
+  <example-component title="{{ __('Practice').'「'.$drill->title.'」' }}" :drill="{{$drill}}" category-name="{{ $drill->category_name }}"></example-component>
   </div>
 @endsection
